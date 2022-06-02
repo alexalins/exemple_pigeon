402dfb21be78e764c826a94081bd531eed7ad10f
alexalins <alexa.glinsoo@gmail.com>
2022-06-02 19:17:54 -0300

    implementando metodo flutter no android

--- android/app/src/main/kotlin/com/example/exemple_pigeon/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/exemple_pigeon/MainActivity.kt
@@ -1,6 +1,28 @@
 package com.example.exemple_pigeon
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.Pigeon
+import io.flutter.plugins.Pigeon.*
+import java.util.*
+
 
 class MainActivity: FlutterActivity() {
+
+    private class MyBookApi: Pigeon.BookApi {
+        override fun search(keyword: String): MutableList<Book> {
+            val random = Random()
+            val book = Pigeon.Book()
+            book.image = "https://source.unsplash.com/random/?book?sig=" + random.nextInt()
+            book.title = "Life " + keyword
+
+            return Collections.singletonList(book)
+        }
+
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        Pigeon.BookApi.setup(flutterEngine.dartExecutor.binaryMessenger, MyBookApi())
+    }
 }
